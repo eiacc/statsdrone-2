4f5f2787c48f825fc35f164afa0537117b9df8fe
ehy  thatsuyha jake luena <eiacc@ehys-MacBook-Air.local>
2025-08-06 09:14:13 +0800

    update: new pages annotations, bonuses, brands, health

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html}"],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,html}',
+    './index.html'
+  ],
   theme: {
     extend: {
       backgroundImage: {
